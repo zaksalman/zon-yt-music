2ebdd8e9cbc70d6ca4194f796fbe90317036819d
zaksalman <jojozon@naver.com>
2024-07-24 13:47:16 +0900

    add

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -4,12 +4,26 @@ import Logo from "./elements/Logo";
 import Navigator from "./elements/Navigator";
 import { cn } from '@/lib/utils';
 
-const Sidebar = () => {
+const Sidebar = ({children}) => {
+  const {isVisiblePlayer} = usePlayerState();
   return (
-    <div>
-      
+    <div
+      className={cn(
+        "flex flex-row h-full",
+        isVisibleState && "h-[calc(100vh-72px)]"
+      )}
+    >
+      <nav className="hidden lg:block w-[240px] border-r-[1px] border-neutral-600"  >
+        <div className="p-[24px]">
+          <Logo/>
+        </div>
+        <div>
+          <Navigator />
+        </div>
+      </nav>
+      <div className="w-full lg:w-[calc(100%-240px)]">{children}</div>
     </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
--- components/elements/PlayListNav.jsx
+++ components/elements/PlayListNav.jsx
@@ -12,11 +12,22 @@ const PlayListNav = ({playlist}) => {
   }
 
   return (
-
-    <div>
-      //TODO
-    </div>
+    <li className="mt-3 px-4 h-[56px] flex flex-col justify-between items-center hover:bg-neutral-700 rounded-lg group">
+      <div>
+        <div className="text-[14px]">{playlistName}</div>
+        <div className="text-[12px] text-neutral-500">{owner}</div>
+      </div>
+      <div>
+        <div
+          onClick={onClickPlay}
+          className="hidden group-hover:block cursor-pointer"
+        >
+          <IoMdPlayCircle size={30}/>
+        </div>
+        </div>
+    </li>
   )
-}
-
+    
+      
+};
 export default PlayListNav
